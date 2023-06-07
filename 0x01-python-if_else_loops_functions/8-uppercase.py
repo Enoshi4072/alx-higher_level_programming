2cc452dd7055f0d01591daf682bf2285774df3c2
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-07 13:27:20 +0000

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in str:
+        value = ord(i)
+        if value >= 97 and value <= 122:
+            value = value - 32
+        print("{:c}".format(value), end='')
+    print("")
