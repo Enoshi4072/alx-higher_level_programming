1e85437ffdc9d7e55fed9790f143393807528bf1
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-08 13:41:58 +0000

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,8 +3,5 @@ import add_0
 a = 1
 b = 2
 answer = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, answer))
-"""
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, answer))
-    """
