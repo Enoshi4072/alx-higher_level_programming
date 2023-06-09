fb7a6d7ef087b07dad3a4b69f26ad423363bce38
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-09 15:50:08 +0000

    2. Replace element mandatory Write a function that replaces an element of a list at a specific position (like in C).
    Prototype: def replace_in_list(my_list, idx, element):
    If idx is negative, the function should not modify anything, and returns the original list
    If idx is out of range (> of number of element in my_list), the function should not modify anything, and returns the original list
    You are not allowed to import any module
    You are not allowed to use try/except

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     length = len(my_list)
-    if idx > length:
+    if idx > length - 1:
         return my_list
     new_list[idx] = element
     return new_list
