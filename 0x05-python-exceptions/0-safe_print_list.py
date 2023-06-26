b7de14d56fca1e83512b41a8e6f2a4603c4c3140
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-26 12:35:34 +0000

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for j in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            i = i + 1
+        except IndexError:
+            break
+    print("")
+    return i
