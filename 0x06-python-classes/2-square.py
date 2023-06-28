7e60f11e2702ef4be1a489bd4c323fb0badac20a
Enoshi4072 <enockkibet6642@gmail.com>
2023-06-28 07:58:15 +0000

    Write a class Square that defines a square by: (based on 1-square.py)
    Private instance attribute: size
    Instantiation with optional size: def __init__(self, size=0):
    size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    if size is less than 0, raise a ValueError exception with the message size must be >= 0
    You are not allowed to import any module

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Defines a class Square."""
+
+
+class Square:
+    """ Code of the square goes here """
+
+    def __init__(self, size=0):
+        """ Initialization of a square
+
+        Args:
+            size (int, optional): The squares size, default is set to 0
+        Errors raised:
+            ValueError: If size < 0.
+            TypeError: If the value provided for size is not an integer.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an interger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
