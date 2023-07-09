19656ee34eff28c1676657a0cb3ddee36518673d
Enoshi4072 <enockkibet6642@gmail.com>
2023-07-09 07:50:12 +0000

    9. A square is a rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -111,7 +111,9 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ("")
 
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
+        return "\n".join(
+                [str(self.print_symbol) * self.__width] * self.__height
+                )
 
     def __repr__(self):
         """
