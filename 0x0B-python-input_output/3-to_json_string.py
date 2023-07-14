ee2ac2500eac8e1b4b1ad88e4c327aa45713be1a
Enoshi4072 <enockkibet6642@gmail.com>
2023-07-14 17:02:07 +0000

    3. To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+
 import json
+""" Returns the JSON representation of an object (string) """
+
 
 def to_json_string(my_obj):
+    """ Utilization of the dumps function for conversion """
+
     j_string = json.dumps(my_obj)
     return j_string
