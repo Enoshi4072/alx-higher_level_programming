9ab103bf258234539f6d88cb2440693f2e77174a
Enoshi4072 <enockkibet6642@gmail.com>
2023-07-18 06:57:07 +0000

    11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,17 +4,51 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square class rep a square shape which is inherited
+    from the rect class
+
+    Args:
+        size: The size of the square (int)
+        x (optional, int): The x-coordinate of the square. Set to 0 as default
+        y (optional, int): The y-coordinate of the square. Default is 0
+        id (int, optional): The unique identifier for the square.
+                            Default set to None
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initializing the square instances """
+        """
+        Initializing the square instances
+
+        Args:
+            size (int): The length of one side of the square
+            x (optional, int): The x-coordinate of the square. Set to 0 as default
+            y (optional, int): The y-coordinate of the square. Default is 0
+            id (int, optional): The unique identifier for the square.
+                                Default set to None
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ Returning a formatted rep of the square """
+        """
+        Returning a formatted rep of the square
+
+        Returns:
+            str: A str rep containing the squares info:
+
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        """ Get the size attribute """
+        """
+        Get the size attribute
+
+        Retrives the side of the square
+
+        Return:
+            int:
+                The side length of the square
+        """
         return self.width
 
     @size.setter
@@ -29,7 +63,13 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ Assign the attributes """
+        """
+        Updates the squares attributes
+
+        Args:
+            *args: A variable arglist containing values (tuple)
+            **kwargs: A variable-length keyword arg list
+        """
         if args and len(args) > 0:
             attr = ['id', 'size', 'x', 'y']
             for i, arg in enumerate(args):
