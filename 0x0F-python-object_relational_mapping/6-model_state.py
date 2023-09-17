750e8704baa872c5e7afefcc5a6bddc5f6eba4e5
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-17 17:41:24 +0000

    8. First state

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -10,5 +10,9 @@ if __name__ == "__main__":
     db_username = sys.argv[1]
     db_password = sys.argv[2]
     db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(db_username, db_password, db_name), pool_pre_ping=True)
+    try:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(db_username, db_password, db_name), pool_pre_ping=True)
+    except Exception as e:
+        print(f"An error occurred while creating the engine: {str(e)}")
+
     Base.metadata.create_all(engine)
