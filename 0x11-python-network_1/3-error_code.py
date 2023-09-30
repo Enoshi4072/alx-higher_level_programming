097ff41e6fc92b21b3a144a84300a4f569f04f67
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 06:45:24 +0000

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8). """
-import urllib.request
-import sys
 if __name__ == "__main__":
+    import urllib.request
+    import sys
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
