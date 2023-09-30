3e328bd6a99f66f2f98bafde64a7611cc316b34e
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 06:42:09 +0000

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,10 +3,11 @@
 and displays the body of the response (decoded in utf-8). """
 import urllib.request
 import sys
-url = sys.argv[1]
-try:
-    with urllib.request.urlopen(url) as response:
-        html = response.read().decode('utf-8')
-        print(html)
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')
+            print(html)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
