6bde94479405f2113494ebe616b8aafc18d1873d
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 06:57:53 +0000

    What is my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ a Python script that fetches
-https://alx-intranet.hbtn.io/status """
-import requests
-url = 'https://alx-intranet.hbtn.io/status'
-try:
-    response = requests.get(url)
-    print("Body response:")
-    print("\t - type: {}".format(type(response.text)))
-    print("\t - content: {}".format(response.text))
-except Exception as e:
-    print(e)
+https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import requests
+    url = 'https://alx-intranet.hbtn.io/status'
+    try:
+        response = requests.get(url)
+        print("Body response:")
+        print("\t - type: {}".format(type(response.text)))
+        print("\t - content: {}".format(response.text))
+    except Exception as e:
+        print(e)
