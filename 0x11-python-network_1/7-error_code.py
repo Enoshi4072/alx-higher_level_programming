130652ad9788f509d8d17c6669ff296e9a0d48d2
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 07:16:09 +0000

    Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,12 +3,13 @@
 and displays the body of the response. """
 import requests
 import sys
-url = sys.argv[1]
-try:
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
-        print(response.text)
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        response = requests.get(url)
+        if response.status_code >= 400:
+            print("Error code:", response.status_code)
+        else:
+            print(response.text)
+    except Exception as e:
+        print(e)
