77419b234196af0fdb1f971c8924805f34d8e8f9
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 07:11:10 +0000

    6. POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,11 +5,12 @@ the email as a parameter,
 and finally displays the body of the response. """
 import sys
 import requests
-url = sys.argv[1]
-email = sys.argv[2]
-email_info = {'email': email}
-try:
-    response = requests.post(url, data=email_info)
-    print(response.text)
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    email_info = {'email': email}
+    try:
+        response = requests.post(url, data=email_info)
+        print(response.text)
+    except Exception as e:
+        print(e)
