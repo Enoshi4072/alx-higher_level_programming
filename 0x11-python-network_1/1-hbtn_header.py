e6000fe9006a2e18c6c83cba94631945eca9e69e
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 06:35:47 +0000

    1.r

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,11 @@
 """A script that: takes in a URL, sends a request to the URL """
 import urllib.request
 import sys
-the_url = sys.argv[1]
-try:
-    with urllib.request.urlopen(the_url) as response:
-        x_request = response.getheader('X-Request-Id')
-    print(x_request)
-except Exception as exp:
-    print(exp)
+if __name__ == "__main__":
+    the_url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(the_url) as response:
+            x_request = response.getheader('X-Request-Id')
+            print(x_request)
+    except Exception as exp:
+        print(exp)
