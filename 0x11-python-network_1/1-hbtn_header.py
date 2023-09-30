2167003e2b27147bbfa8072512f718e426b9f8b7
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 06:30:28 +0000

    1.r

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL, sends a request to the URL
-- and displays the value of the X-Request-Id
-- variable found in the header of the response.
-"""
+"""A script that: takes in a URL, sends a request to the URL """
 import urllib.request
 import sys
 the_url = sys.argv[1]
@@ -13,3 +9,6 @@ try:
     print(x_request)
 except Exception as exp:
     print(exp)
+
+if __name__ == "__main__":
+    main()
