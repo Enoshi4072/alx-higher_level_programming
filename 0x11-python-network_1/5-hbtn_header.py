ddeac81da5bcbe3c6926bd3a0f8226fe3010df5a
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 07:04:06 +0000

    5. Response

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" takes in a URL, sends a request to the URL
+"""
+takes in a URL, sends a request to the URL
 and displays the value of the variable
-X-Request-Id in the response header """
-import requests
-import sys
-url = sys.argv[1]
-try:
-    x_request = requests.get(url)
-    if 'X-Request-Id' in x_request.headers:
-        x_req = x_request.headers['X-Request-Id']
-        print(x_req)
-except Exception as e:
-    print(e)
+X-Request-Id in the response header
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    try:
+        x_request = requests.get(url)
+        if 'X-Request-Id' in x_request.headers:
+            x_req = x_request.headers['X-Request-Id']
+            print(x_req)
+    except Exception as e:
+        print(e)
