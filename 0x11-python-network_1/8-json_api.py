f384b3dcf8c9c0491d809528f7a6b3fa3d2db85e
Enoshi4072 <enockkibet6642@gmail.com>
2023-09-30 07:37:44 +0000

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,22 +4,23 @@ to http://0.0.0.0:5000/search_user
 with the letter as a parameter. """
 import requests
 import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
 
-if len(sys.argv) == 1:
-    q = ""
-q = sys.argv[1]
-
-q_data = {'q': q}
-url = 'http://0.0.0.0:5000/search_user'
-try:
-    response = requests.post(url, q_data=data)
+    q_data = {'q': q}
+    url = 'http://0.0.0.0:5000/search_user'
     try:
-        json_d = response.json()
-        if json_d:
-            print("[{}] {}".format(json_d['id'], json_d['name']))
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
-except Exception as e:
-    print("No result")
+        response = requests.post(url, data=q_data)
+        try:
+            json_d = response.json()
+            if json_d:
+                print("[{}] {}".format(json_d['id'], json_d['name']))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    except Exception as e:
+        print("No result")
