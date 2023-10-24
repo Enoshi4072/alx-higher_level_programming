4f5a4d7fa6707b6463c497ce19441cba2b71701c
Enoshi4072 <enockkibet6642@gmail.com>
2023-10-24 14:38:51 +0000

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,22 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
+  // Added error handling for the request
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
   const data = JSON.parse(body);
   const wedgeAntillesID = 18;
 
+  // Added check for valid data format
+  if (!data.results) {
+    console.error('Invalid data format');
+    process.exit(1);
+  }
+
+  // Filter movies based on characters including Wedge Antilles
   const moviesWithWedgeAntilles = data.results.filter(movie =>
     movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesID}/`)
   );
